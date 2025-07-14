3a4b37301999dc9d0ee83ca58bd7317e36eb39d8
Pascal Peters <P.Peters@FruVa.de>
2025-07-14 13:04:14 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using ToDoApp.ViewModels;
+
+namespace ToDoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ToDoApp.ViewModels
+{
+    internal partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _title = "To-Do List Application";
+    }
+}
