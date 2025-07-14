2ceb3fc3b5509a74cb8af450c872e19914b6edf4
Pascal Peters <P.Peters@FruVa.de>
2025-07-14 14:03:57 +0200

    Completed

---
+++ Models/ToDo.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ToDoApp.Models;
+
+public partial class ToDo : ObservableObject
+{
+    [ObservableProperty]
+    private string? _title;
+
+    [ObservableProperty]
+    private string? _description;
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,56 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ToDoApp.Models;
 
-namespace ToDoApp.ViewModels
+namespace ToDoApp.ViewModels;
+
+public partial class MainWindowViewModel : ObservableObject
 {
-    internal partial class MainWindowViewModel : ObservableObject
+    [ObservableProperty]
+    private string? _title;
+
+    [ObservableProperty]
+    private string? newTitle;
+
+    [ObservableProperty]
+    private string? newDescription;
+
+    [ObservableProperty]
+    private ToDo? selectedToDo;
+
+    public ObservableCollection<ToDo> OpenToDos { get; } = [];
+    public ObservableCollection<ToDo> CompletedToDos { get; } = [];
+
+    [RelayCommand]
+    private void AddToDo()
     {
-        [ObservableProperty]
-        private string _title = "To-Do List Application";
+        if (!string.IsNullOrWhiteSpace(NewTitle))
+        {
+            OpenToDos.Add(new ToDo { Title = NewTitle, Description = NewDescription });
+            NewTitle = string.Empty;
+            NewDescription = string.Empty;
+        }
+    }
+
+    [RelayCommand]
+    private void DeleteToDo()
+    {
+        if (SelectedToDo != null)
+        {
+            OpenToDos.Remove(SelectedToDo);
+            SelectedToDo = null;
+        }
+    }
+
+    [RelayCommand]
+    private void CompleteToDo()
+    {
+        if (SelectedToDo != null)
+        {
+            CompletedToDos.Add(SelectedToDo);
+            OpenToDos.Remove(SelectedToDo);
+            SelectedToDo = null;
+        }
     }
 }
